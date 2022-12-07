39c6cde012212a459c3472f37e3c5bb7444cdb0d
shivrajpro <shivrajhulbatte@gmail.com>
2022-12-07 19:36:00 +0530

    get and update user status

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const { body } = require("express-validator");
 const User = require("../models/user");
 const authController = require('../controllers/auth');
 const router = express.Router();
+const isAuth = require('../middleware/is-auth');
 
 router.put("/signup", [
   body("email")
@@ -22,4 +23,8 @@ router.put("/signup", [
 );
 
 router.post('/login', authController.login);
+router.get('/status', isAuth, authController.getUserStatus);
+router.patch('/status', isAuth, body('status').trim().notEmpty(),
+authController.updateUserStatus);
+
 module.exports = router;
