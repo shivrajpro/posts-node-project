dcd0db4fa8226bc694d0f224c94812cc179a3bc1
shivrajpro <shivrajhulbatte@gmail.com>
2022-12-08 10:52:21 +0530

    converting promises in auth.js to async await

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,9 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-exports.signup = (req, res, next) => {
+exports.signup = async (req, res, next) => {
   const email = req.body.email;
   const username = req.body.username;
   const password = req.body.password;
@@ -16,106 +16,81 @@ exports.signup = (req, res, next) => {
     throw error;
   }
 
-  bcrypt
-    .hash(password, 12)
-    .then((hashedPw) => {
-      const user = new User({
-        email: email,
-        password: hashedPw,
-        username: username,
-      });
-
-      return user.save();
-    })
-    .then((result) => {
-      res.status(201).json({ message: "User created", userId: result._id });
-    })
-    .catch((e) => {
-      console.log(e);
-      if (!e.statusCode) e.statusCode = 500;
-      next(e);
+  try {
+    const hashedPw = await bcrypt.hash(password, 12);
+    const user = new User({
+      email: email,
+      password: hashedPw,
+      username: username,
     });
+
+    const result = await user.save();
+    res.status(201).json({ message: "User created", userId: result._id });
+  } catch (e) {
+    if (!e.statusCode) e.statusCode = 500;
+    next(e);
+  }
 };
 
-exports.login = (req, res, next)=>{
+exports.login = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  let loadedUser;
 
-  User.findOne({email})
-  .then(user=>{
-    if(!user){
-      const error = new Error("User does not exist with that email");
-      error.statusCode = 422;
-      throw error;
-    }
+  const user = await User.findOne({ email });
+  if (!user) {
+    const error = new Error("User does not exist with that email");
+    error.statusCode = 422;
+    throw error;
+  }
+  const isEqual = await bcrypt.compare(password, user.password);
 
-    loadedUser = user;
+  if (!isEqual) {
+    const error = new Error("Wrong password!");
+    error.statusCode = 422;
+    throw error;
+  }
 
-    return bcrypt.compare(password, user.password);
-  })
-  .then(isEqual=>{
-    if(!isEqual){
-      const error = new Error("Wrong password!");
-      error.statusCode = 422;
-      throw error;
-    }
+  const token = jwt.sign(
+    { email: user.email, userId: user._id.toString() },
+    "somesupersecretsecret",
+    { expiresIn: "1h" }
+  );
 
-    const token = jwt.sign(
-      { email: loadedUser.email, 
-        userId: loadedUser._id.toString() 
-      },
-      'somesupersecretsecret',
-      {expiresIn:'1h'}
-      )
-    
-      res.status(200).json({token, userId: loadedUser._id.toString()});
-  })
-  .catch(e=>{
-    // console.log(e);
-    next(e)
-  })
-}
+  res.status(200).json({ token, userId: user._id.toString() });
+};
 
-
-exports.getUserStatus = (req, res, next)=>{
-  User.findById(req.userId)
-  .then(user=>{
-    if(!user){
+exports.getUserStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
       const error = new Error("User not found");
       error.statusCode = 404;
       throw error;
     }
 
-    res.status(200).json({status:user.status})
-
-  })
-  .catch(e=>{
+    res.status(200).json({ status: user.status });
+  } catch (e) {
     if (!e.statusCode) e.statusCode = 500;
     next(e);
-  })
-}
+  }
+};
 
-exports.updateUserStatus = (req, res, next)=>{
+exports.updateUserStatus = async (req, res, next) => {
   const newStatus = req.body.status;
 
-  User.findById(req.userId)
-  .then(user=>{
-    if(!user){
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
       const error = new Error("User not found");
       error.statusCode = 404;
       throw error;
     }
 
     user.status = newStatus;
-    return user.save();
-    
-  })
-  .then(result=>{
-    res.status(200).json({message:"Status updated!"})
-  })
-  .catch(e=>{
+    const result = await user.save();
+    res.status(200).json({ message: "Status updated!" });
+  } catch (e) {
     if (!e.statusCode) e.statusCode = 500;
     next(e);
-  })  
-}
+  }
+};
